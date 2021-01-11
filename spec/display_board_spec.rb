2ce62941f8ccd034bba778dbdf0e5cc59cf52dd0
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-11 19:00:17 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -176,7 +176,7 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Os' do
-      board = ["O", "O", "O", "O", "O", "O", "O", "O", ""] # This is not correct
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"] # This is not correct
 
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
